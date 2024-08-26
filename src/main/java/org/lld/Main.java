c1446b4c7b7363fa9740f9cbd24633bc7badaf27
Anadi Tiwari <anadi.tiwari@housing.com>
2024-08-26 18:47:32 +0530

    builder design pattern

--- src/main/java/org/lld/Main.java
+++ src/main/java/org/lld/Main.java
@@ -3,14 +3,14 @@ package org.lld;
 
 public class Main {
     public static void main(String[] args) {
-        //User user = new User.UserBuilder().setUserName("fuck").setUserId("you").build();
-        User user = User.builder().setUserName("fuck").setUserId("you").build();
-        System.out.println(user.toString());
 
-        //WorkerDetailsToPushOnFlatsPojo
-          //      .builder()
-            //    .workerName("FlatReactivationMetricWorker")
-              //  .args(Arrays.asList(flat.getId()))
-               // .build());
+        /**
+           BUILDER DESIGN PATTERN
+          //User user = new User.UserBuilder().setUserName("fuck").setUserId("you").build();
+          User user = User.builder().setUserName("fuck").setUserId("you").build();
+          System.out.println(user.toString());
+        */
+
+
     }
 }
